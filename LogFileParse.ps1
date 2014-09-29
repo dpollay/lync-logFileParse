8bdc3c4a3e0541751dfb9eee27df78c12a2cff1d
David Pollay <dpollay@pollay.com>
2014-09-29 11:12:58 -0400

    Version 2
    Broke output into multiple sections for easier reading

--- LogFileParse.ps1
+++ LogFileParse.ps1
@@ -1,28 +1,27 @@
-﻿#################################################################################
-#  Lync Log File Parser
-#  Copyright (C) 2014 David Pollay
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#################################################################################
+﻿<#
+Lync Log File Parser
+Copyright (C) 2014 David Pollay
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
-$outfileDir = $env:USERPROFILE
-$htmlfile = $env:USERPROFILE + '\LyncLog.html'
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-out-file $htmlfile
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-# HTML Formatting
-$Header = @"
+#>
+function Lync-ParseLog{
+
+Begin{
+    $htmlfile = $env:HOME + '\LyncLog.html'
+    out-file $htmlfile
+    $Header = @"
 <style>
 TABLE {border-width: 1px;border-style: solid;border-color: black;border-collapse: collapse;}
 TH {border-width: 1px;padding: 3px;border-style: solid;border-color: black;background-color: #6495ED;}
@@ -32,44 +31,65 @@ TD {border-width: 1px;padding: 3px;border-style: solid;border-color: black;}
 TR:Nth-Child(Even) {Background-Color: #dddddd;}
 TR:Hover TD {Background-Color: #C1D5F8;}
 </style>
+<center>
 <title>
-Lync Log File Parse
+Lync Log File Parsing
 </title>
 "@
+}
 
-# Load Lync log files and grab each line that contains a VQ Report
-select-String $env:LocalAppData\Microsoft\Office\15.0\Lync\Tracing\*.UccApilog* -pattern "<vqreport" |
-    # Convert output into XML Data
+Process{ $endpointstats = select-String $env:LocalAppData\Microsoft\Office\15.0\Lync\Tracing\*.UccApilog* -pattern "<vqreport" |
     ForEach-Object {
         $check =  $_.Line -match "</VQReportEvent>"
         if($check -NotMatch "True"){
             $_.Line += "</VQSessionReport></VQReportEvent>"
         }
         $xmldoc = [xml]$_.Line
-
-        # Choose Fields to Grab
-        $information = $_ | Select-Object -Property Name, OS, CPUName, CPUNumberOfCores, `
-            Start, End, FromURI, ToURI, LocalUserAgent, `
-            AUdioCodec, AudioCaptureDevName, AudioCaptureDevDriver, AudioRenderDevName, AudioRenderDevDriver, RecvListenMOS, `
-            MainVideoCodec, MainVideoCaptureDevName, MainVideoCaptureDevDriver, MainVideoRenderDevName, MainVideoRenderDevDriver, mainVideoResolution, mainVideoFrameRateAvg, `
-            mainVideoCifQuality, mainVideoVGAQuality, mainVideoHDQuality
-
-        # Grab Endpoint Statistics
+        $information = $_ | Select-Object -Property Name, OS, CPUName, CPUNumberOfCores
         $endpointChoices = "Name", "OS", "CPUName", "CPUNumberOfCores"
         $endpoint = $xmldoc.VQReportEvent.VQSessionReport.Endpoint
-        foreach($endpointchoice in $endpointChoices){
-            $information.$endpointchoice = $endpoint.$endpointchoice
-        }
+        $information.Name = $endpoint.Name
+        $information.OS = $endpoint.OS
+        $information.CPUName = $endpoint.CPUName
+        $information.CPUNumberOfCores = $endpoint.CPUNumberOfCores
+        $information
+    } | select @{name="Computer Name";expression={$information.Name}}, `
+    @{name="Operating System";expression={$information.OS}}, `
+    @{name="CPU Name";expression={$information.CPUName}}, `
+    @{name="# of CPU Cores";expression={$information.CPUNumberOfCores}} `
+    | ConvertTo-Html -Fragment -As Table -PreContent "<h2>Computer Information</h2>" | Out-String
 
-        # Grab DialogInfo Statistics
-        $dialogChoices = "Start", "End", "FromURI", "ToURI", "LocalUserAgent"
+$dialoginfostats = select-String $env:LocalAppData\Microsoft\Office\15.0\Lync\Tracing\*.UccApilog* -pattern "<vqreport" |
+    ForEach-Object {
+        $check =  $_.Line -match "</VQReportEvent>"
+        if($check -NotMatch "True"){
+            $_.Line += "</VQSessionReport></VQReportEvent>"
+        }
+        $xmldoc = [xml]$_.Line
+        $information = $_ | Select-Object -Property Start, End, FromURI, ToURI, LocalUserAgent
         $dialog = $xmldoc.VQReportEvent.VQSessionReport.DialogInfo
-        foreach($dialogchoice in $dialogChoices){
-            $information.$dialogchoice = $dialog.$dialogchoice
-        }
+        $information.Start = $dialog.Start
+        $information.End = $dialog.End
+        $information.FromURI = $dialog.FromURI
+        $information.ToURI = $dialog.ToURI
+        $information.LocalUserAgent = $dialog.LocalUserAgent
+        $information
+    } | select @{name="Start Time";expression={[datetime]$information.Start}}, `
+    @{name="End Time";expression={[datetime]$information.End}}, `
+    @{name="From User";expression={$information.FromURI}}, `
+    @{name="To User";expression={$information.ToURI}}, `
+    @{name="Lync Version";expression={$information.LocalUserAgent}} `
+    | ConvertTo-Html -Fragment -As Table -PreContent "<h2>Call Setup Information</h2>" | Out-String
 
-        # Grab MediaLine main-audio Statistics
-        if($information.ToURI -notmatch "applicationsharing"){
+$audiostats = select-String $env:LocalAppData\Microsoft\Office\15.0\Lync\Tracing\*.UccApilog* -pattern "<vqreport" |
+    ForEach-Object {
+        $check =  $_.Line -match "</VQReportEvent>"
+        if($check -NotMatch "True"){
+            $_.Line += "</VQSessionReport></VQReportEvent>"
+        }
+        $xmldoc = [xml]$_.Line
+        $information = $_ | Select-Object -Property AudioCodec, AudioCaptureDevName, AudioCaptureDevDriver, AudioRenderDevName, AudioRenderDevDriver, RecvListenMOS
+        if($xmldoc.VQReportEvent.VQSessionReport.DialogInfo.ToURI -notmatch "applicationsharing"){
             $mainaudio = $xmldoc.VQReportEvent.VQSessionReport.MediaLine
             $information.AudioCodec = $mainaudio.InboundStream.Payload.Audio.PayloadDescription
             $information.AudioCaptureDevName = $mainaudio.Description.CaptureDev.Name[0]
@@ -78,9 +98,33 @@ select-String $env:LocalAppData\Microsoft\Office\15.0\Lync\Tracing\*.UccApilog* 
             $information.AudioRenderDevDriver = $mainaudio.Description.RenderDev.Driver[0]
             $information.RecvListenMOS = $mainaudio.InboundStream.QualityEstimates.Audio.RecvListenMOS
         }
+        else{
+            $information.AudioCodec = "N/A"
+            $information.AudioCaptureDevName = "N/A"
+            $information.AudioCaptureDevDriver = "N/A"
+            $information.AudioRenderDevName = "N/A"
+            $information.AudioRenderDevDriver = "N/A"
+            $information.RecvListenMOS = "N/A"
+        }
+        $information
+    } | select @{name="Codec";expression={$information.AudioCodec}}, `
+    @{name="Capture Device";expression={$information.AudioCaptureDevName}}, `
+    @{name="Capture Driver";expression={$information.AudioCaptureDevDriver}}, `
+    @{name="Render Device";expression={$information.AudioRenderDevName}}, `
+    @{name="Render Driver";expression={$information.AudioRenderDevDriver}}, `
+    @{name="MOS";expression={$information.RecvListenMOS}} `
+    | ConvertTo-Html -Fragment -As Table -PreContent "<h2>Audio Information</h2>" | Out-String
 
-        # Grab MediaLine main-MainVideo Statistics
-        if($information.ToURI -notmatch "applicationsharing"){
+$videostats = select-String $env:LocalAppData\Microsoft\Office\15.0\Lync\Tracing\*.UccApilog* -pattern "<vqreport" |
+    ForEach-Object {
+        $check =  $_.Line -match "</VQReportEvent>"
+        if($check -NotMatch "True"){
+            $_.Line += "</VQSessionReport></VQReportEvent>"
+        }
+        $xmldoc = [xml]$_.Line
+        $information = $_ | Select-Object -Property MainVideoCodec, MainVideoCaptureDevName, MainVideoCaptureDevDriver, MainVideoRenderDevName, MainVideoRenderDevDriver, mainVideoResolution, mainVideoFrameRateAvg, `
+            mainVideoCifQuality, mainVideoVGAQuality, mainVideoHDQuality
+        if($xmldoc.VQReportEvent.VQSessionReport.DialogInfo.ToURI -notmatch "applicationsharing"){
             $MainVideo = $xmldoc.VQReportEvent.VQSessionReport.MediaLine
             $information.MainVideoCodec = $MainVideo.InboundStream.Payload.Video.RecvCodecTypes[0]
             $information.MainVideoCaptureDevName = $MainVideo.Description.CaptureDev.Name[1]
@@ -93,30 +137,37 @@ select-String $env:LocalAppData\Microsoft\Office\15.0\Lync\Tracing\*.UccApilog* 
             $information.MainVideoVGAQuality = $MainVideo.InboundStream.Payload.Video.VideoResolutionDistribution.VGAQuality[0]
             $information.MainVideoHDQuality = $MainVideo.InboundStream.Payload.Video.VideoResolutionDistribution.HD720Quality[0]
         }
+        else{
+            $information.MainVideoCodec = "N/A"
+            $information.MainVideoCaptureDevName = "N/A"
+            $information.MainVideoCaptureDevDriver = "N/A"
+            $information.MainVideoRenderDevName = "N/A"
+            $information.MainVideoRenderDevDriver = "N/A"
+            $information.MainVideoResolution = "N/A"
+            $information.MainVideoFrameRateAvg = "N/A"
+            $information.MainVideoCifQuality = "N/A"
+            $information.MainVideoVGAQuality = "N/A"
+            $information.MainVideoHDQuality = "N/A"
+        }
+        $information
+    } | select @{name="Codec";expression={$information.MainVideoCodec}}, `
+    @{name="Capture Device";expression={$information.MainVideoCaptureDevName}}, `
+    @{name="Capture Driver";expression={$information.MainVideoCaptureDevDriver}}, `
+    @{name="Render Device";expression={$information.MainVideoRenderDevName}}, `
+    @{name="Render Driver";expression={$information.MainVideoRenderDevDriver}}, `
+    @{name="Avg FPS";expression={$information.MainVideoFrameRateAvg}}, `
+    @{name="Resolution";expression={$information.MainVideoResolution}}, `
+    @{name="% CIF Quality";expression={$information.MainVideoCifQuality}}, `
+    @{name="% VGA Quality";expression={$information.MainVideoVGAQuality}}, `
+    @{name="% HD Quality";expression={$information.MainVideoHDQuality}} `
+    | ConvertTo-Html -Fragment -As Table -PreContent "<h2>Video Information</h2>" | Out-String
 
+$Report = ConvertTo-Html -Title "Lync Log File Parsing" `
+    -Head $header `
+    -Body "$endpointstats $dialoginfostats $audiostats $videostats"
+}
 
-        $information 
-    } | select @{name="Start Time";expression={[datetime]$information.Start}}, ` 
-        @{name="End Time";expression={[datetime]$information.End}}, `
-        @{name="From User";expression={$information.FromURI}}, `
-        @{name="To User";expression={$information.ToURI}}, `
-        @{name="Lync Version";expression={$information.LocalUserAgent}}, `
-        @{name="Audio Codec";expression={$information.AudioCodec}}, `
-        @{name="Audio Capture Name";expression={$information.AudioCaptureDevName}}, `
-        @{name="Audio Capture Driver";expression={$information.AudioCaptureDevDriver}}, `
-        @{name="Audio Render Name";expression={$information.AudioRenderDevName}}, `
-        @{name="Audio Render Driver";expression={$information.AudioRenderDevDriver}}, `
-        @{name="Audio MOS";expression={$information.RecvListenMos}}, `
-        @{name="Main Video Codec";expression={$information.MainVideoCodec}}, `
-        @{name="Main Video Capture Name";expression={$information.MainVideoCaptureDevName}}, `
-        @{name="Main Video Capture Driver";expression={$information.MainVideoCaptureDevDriver}}, `
-        @{name="Main Video Render Name";expression={$information.MainVideoRenderDevName}}, `
-        @{name="Main Video Render Driver";expression={$information.MainVideoRenderDevDriver}}, `
-        @{name="Main Video Resolution";expression={$information.MainVideoResolution}}, `
-        @{name="Main Video FPS Avg";expression={$information.MainVideoFrameRateAvg}}, `
-        @{name="Main Video % CIF Quality";expression={$information.MainVideoCifQuality}}, `
-        @{name="Main Video % VGA Quality";expression={$information.MainVideoVGAQUality}}, `
-        @{name="Main Video % HD Quality";expression={$information.MainVideoHDQuality}} `
-        | ConvertTo-Html -head $header | Out-File $htmlfile -append
+End{$report | out-file $htmlfile ; Invoke-Expression $htmlfile}
 
-Invoke-Expression $htmlfile
+}
+Lync-ParseLog
